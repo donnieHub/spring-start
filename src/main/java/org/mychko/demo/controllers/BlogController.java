08d51c3dd6cff7edb21ec84d79653cd102ab13cd
mychko <v.mychko@premiumitsolution.com>
2021-11-17 23:00:19 +0300

    add detailed post view template

--- src/main/java/org/mychko/demo/controllers/BlogController.java
+++ src/main/java/org/mychko/demo/controllers/BlogController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -34,5 +37,16 @@ public class BlogController {
         Post post = new Post(title, anons, full_text);
         postRepository.save(post);
         return "redirect:/";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogGetPost(@PathVariable(value = "id") long id, Model model) {
+        Optional<Post> optionalPost = postRepository.findById(id);
+        if (optionalPost.isPresent()){
+            model.addAttribute("post", optionalPost.get());
+        } else {
+            return "redirect:/";
+        }
+        return "blog-details";
     }
 }
